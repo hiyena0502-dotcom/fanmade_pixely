85e51bfd9d606cfb5b230a3cd6098267303b9569
hiyena0502-dotcom <hiyena0502@gmail.com>
2026-09-14 18:09:52 +0900

    Bump CLICK UX script cache version

--- collection-groups.js
+++ collection-groups.js
@@ -1,6 +1,6 @@
 (() => {
   const game = document.createElement('script');
-  game.src = 'pixely-game.js?v=6';
+  game.src = 'pixely-game.js?v=10';
   game.async = false;
   document.head.appendChild(game);
 
