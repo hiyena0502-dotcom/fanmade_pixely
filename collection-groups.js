7e4be34c9f522422036ca8646ae3b7bec790e4d0
hiyena0502-dotcom <hiyena0502@gmail.com>
2026-09-14 17:57:21 +0900

    Bump CLICK UX test loader version

--- collection-groups.js
+++ collection-groups.js
@@ -1,6 +1,6 @@
 (() => {
   const game = document.createElement('script');
-  game.src = 'pixely-game.js?v=4';
+  game.src = 'pixely-game.js?v=5';
   game.async = false;
   document.head.appendChild(game);
 
