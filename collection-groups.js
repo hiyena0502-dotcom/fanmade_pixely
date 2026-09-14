6fc848de2d7e30a9ccd611a696e4e0fc7688e8fd
hiyena0502-dotcom <hiyena0502@gmail.com>
2026-09-14 18:43:35 +0900

    Decouple collection loader from game scripts

--- collection-groups.js
+++ collection-groups.js
@@ -1,43 +1,32 @@
 (() => {
-  const game = document.createElement('script');
-  game.src = 'pixely-game.js?v=10';
-  game.async = false;
-  game.onload = () => {
-    const gachaUpgrade = document.createElement('script');
-    gachaUpgrade.src = 'gacha-upgrade.js?v=1';
-    gachaUpgrade.async = false;
-    document.head.appendChild(gachaUpgrade);
-  };
-  document.head.appendChild(game);
-
   const theme = document.createElement('link');
   theme.rel = 'stylesheet';
-  theme.href = 'collection-groups.css?v=4';
+  theme.href = 'collection-groups.css?v=6';
   document.head.appendChild(theme);
 
   const css = document.createElement('link');
   css.rel = 'stylesheet';
-  css.href = 'collection-collapse.css?v=4';
+  css.href = 'collection-collapse.css?v=5';
   document.head.appendChild(css);
 
   const core = document.createElement('script');
-  core.src = 'collection-groups-core.js?v=3';
+  core.src = 'collection-groups-core.js?v=4';
   core.async = false;
   core.onload = () => {
     const collapse = document.createElement('script');
-    collapse.src = 'collection-collapse.js?v=3';
+    collapse.src = 'collection-collapse.js?v=4';
     collapse.async = false;
     collapse.onload = () => {
       const unify = document.createElement('script');
-      unify.src = 'collection-unify.js?v=3';
+      unify.src = 'collection-unify.js?v=4';
       unify.async = false;
       unify.onload = () => {
         const routeFix = document.createElement('script');
-        routeFix.src = 'collection-route-fix.js?v=3';
+        routeFix.src = 'collection-route-fix.js?v=4';
         routeFix.async = false;
         routeFix.onload = () => {
           const storyGroups = document.createElement('script');
-          storyGroups.src = 'scenario-groups.js?v=2';
+          storyGroups.src = 'scenario-groups.js?v=3';
           storyGroups.async = false;
           document.head.appendChild(storyGroups);
         };
