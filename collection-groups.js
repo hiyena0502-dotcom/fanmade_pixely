cd7171ee474942e5eae5f12e0f4dc6f7f2f41e96
hiyena0502-dotcom <hiyena0502@gmail.com>
2026-09-14 17:51:24 +0900

    Load clicker game v4

--- collection-groups.js
+++ collection-groups.js
@@ -1,33 +1,33 @@
 (() => {
   const game = document.createElement('script');
-  game.src = 'pixely-game.js?v=3';
+  game.src = 'pixely-game.js?v=4';
   game.async = false;
   document.head.appendChild(game);
 
   const theme = document.createElement('link');
   theme.rel = 'stylesheet';
-  theme.href = 'collection-groups.css?v=3';
+  theme.href = 'collection-groups.css?v=4';
   document.head.appendChild(theme);
 
   const css = document.createElement('link');
   css.rel = 'stylesheet';
-  css.href = 'collection-collapse.css?v=3';
+  css.href = 'collection-collapse.css?v=4';
   document.head.appendChild(css);
 
   const core = document.createElement('script');
-  core.src = 'collection-groups-core.js?v=2';
+  core.src = 'collection-groups-core.js?v=3';
   core.async = false;
   core.onload = () => {
     const collapse = document.createElement('script');
-    collapse.src = 'collection-collapse.js?v=2';
+    collapse.src = 'collection-collapse.js?v=3';
     collapse.async = false;
     collapse.onload = () => {
       const unify = document.createElement('script');
-      unify.src = 'collection-unify.js?v=2';
+      unify.src = 'collection-unify.js?v=3';
       unify.async = false;
       unify.onload = () => {
         const routeFix = document.createElement('script');
-        routeFix.src = 'collection-route-fix.js?v=2';
+        routeFix.src = 'collection-route-fix.js?v=3';
         routeFix.async = false;
         document.head.appendChild(routeFix);
       };
